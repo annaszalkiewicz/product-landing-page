782b1ed0c581d8394dde5da6d3c4ba183e1e6e9e
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-09-17 22:29:30 +0200

    Add google maps to contact section

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const icon = document.getElementById('hamburger-menu');
 const menu = document.getElementById('nav-bar');
 
 const toggleMenu = () => {
-	
+
 	if (menu.classList.contains('show')) {
 		menu.classList.remove('show');
 	}
@@ -14,3 +14,17 @@ const toggleMenu = () => {
 
 };
 icon.addEventListener('click', toggleMenu);
+
+// Create google map
+
+var map;
+
+window.initMap = () => {
+	map = new window.google.maps.Map(document.getElementById('map'), {
+		zoom: 6,
+		center: { lat: 51.7730971, lng: 19.4105533 }
+	});
+	window.google.maps.event.addListenerOnce(map, 'idle', () => {
+		document.getElementsByTagName('iframe')[0].title = 'Google  Maps';
+	});
+};
